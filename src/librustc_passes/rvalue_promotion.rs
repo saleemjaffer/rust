2e2f1454d093463c91066324fa1b6333ee0c2781
Saleem Jaffer <saleem@acko.com>
2019-02-28 13:05:28 +0530

    minor

--- src/librustc_passes/rvalue_promotion.rs
+++ src/librustc_passes/rvalue_promotion.rs
@@ -14,7 +14,7 @@
 // - It's not possible to take the address of a static item with unsafe interior. This is enforced
 // by borrowck::gather_loans
 
-use rustc::ty::cast::CastKind;
+use rustc::ty::cast::CastTy;
 use rustc::hir::def::{Def, CtorKind};
 use rustc::hir::def_id::DefId;
 use rustc::middle::expr_use_visitor as euv;
@@ -319,12 +319,9 @@ fn check_expr_kind<'a, 'tcx>(
         hir::ExprKind::Cast(ref from, _) => {
             let expr_promotability = v.check_expr(from);
             debug!("Checking const cast(id={})", from.hir_id);
-            match v.tables.cast_kinds().get(from.hir_id) {
-                None => {
-                    v.tcx.sess.delay_span_bug(e.span, "no kind for cast");
-                    NotPromotable
-                },
-                Some(&CastKind::PtrAddrCast) | Some(&CastKind::FnPtrAddrCast) => {
+            let cast_in = CastTy::from_ty(v.tables.expr_ty(from)).expect("bad input type for cast");
+            match cast_in {
+                CastTy::FnPtr | CastTy::Ptr(_) => {
                     NotPromotable
                 }
                 _ => expr_promotability
